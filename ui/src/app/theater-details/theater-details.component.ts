cb5d397f5f91cd35215372ee987733b6e2ac0a66
Seethal Elias <seethalelias@gmail.com>
2024-11-22 13:55:35 -0500

    generate component for theaterdetails

---
+++ ui/src/app/theater-details/theater-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-theater-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './theater-details.component.html',
+  styleUrl: './theater-details.component.css'
+})
+export class TheaterDetailsComponent {
+
+}
