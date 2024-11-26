3ef6e8789fa6a516126d61253c88472c553dba38
Neha Tom Merin <nehamerintom@gmail.com>
2024-11-26 09:29:33 -0700

    complete login page

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { MovieDetailsComponent } from './movie-details/movie-details.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'register', component: RegisterPageComponent },
+  { path: 'login', component: LoginPageComponent},
   { path: 'movie-details', component: MovieDetailsComponent },
 ];
