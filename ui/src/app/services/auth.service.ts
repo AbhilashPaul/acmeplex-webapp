305e12de61213cf68ec3f7fdb7203330e466a415
Seethal Elias <seethalelias@gmail.com>
2024-11-28 16:20:42 -0500

    registersaved

--- ui/src/app/services/auth.service.ts
+++ ui/src/app/services/auth.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { Observable } from 'rxjs';
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  private apiUrl = environment.apiUrl;
   private loggedIn = false;
+
+  constructor(private http: HttpClient) {}
 
   login() {
     this.loggedIn = true;
@@ -16,5 +23,9 @@ export class AuthService {
 
   isLoggedIn(): boolean {
     return this.loggedIn;
+  }
+
+  register(userData: any, options: { headers: HttpHeaders }) : Observable<any> {
+    return this.http.post(`${this.apiUrl}/auth/register`, userData, options);
   }
 }
