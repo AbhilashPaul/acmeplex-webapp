314fd7e9517ade1e958f228e56db78446a27d83f
Abhilash Paul <paul.abhilash@hotmail.com>
2024-11-30 00:54:28 -0700

    pass amount as well to the payment gateway

--- api/src/main/java/com/acmeplex/api/service/PaymentGateway.java
+++ api/src/main/java/com/acmeplex/api/service/PaymentGateway.java
@@ -8,7 +8,7 @@ import java.util.UUID;
 @Service
 public class PaymentGateway {
 
-    public String processPayment(PaymentCardDto paymentCardDto){
+    public String processPayment(PaymentCardDto paymentCardDto, Double amount){
         // Simulate transaction processing (e.g., via a third-party gateway)
         return UUID.randomUUID().toString(); // Simulated transaction ID
     }
