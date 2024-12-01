9e994e3da51f94cf2fe8eda160039b3899ded90e
Abhilash Paul <paul.abhilash@hotmail.com>
2024-11-30 22:34:40 -0700

    remove unused controller

--- api/src/main/java/com/acmeplex/api/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.acmeplex.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.acmeplex.api.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
