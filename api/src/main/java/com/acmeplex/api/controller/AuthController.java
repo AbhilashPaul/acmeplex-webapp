a390e646776ef486c3cdbac17fff2ee764bad523
Abhilash Paul <paul.abhilash@hotmail.com>
2024-11-30 22:37:49 -0700

    adds logging in AuthController

--- api/src/main/java/com/acmeplex/api/controller/AuthController.java
+++ api/src/main/java/com/acmeplex/api/controller/AuthController.java
@@ -1,7 +1,10 @@
 package com.acmeplex.api.controller;
 
 import com.acmeplex.api.model.RegisteredUser;
+import com.acmeplex.api.notifier.EmailNotifier;
 import com.acmeplex.api.service.RegisteredUserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +20,7 @@ import java.util.Optional;
 @RequestMapping("/auth")
 @CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
+    Logger logger = LoggerFactory.getLogger(AuthController.class);
 
     @Autowired
     private RegisteredUserService registeredUserService;
@@ -37,8 +41,10 @@ public class AuthController {
         try {
             user.setMemberSince(new Date());
             RegisteredUser savedUser = registeredUserService.createRegisteredUser(user);
+            logger.debug("Successfully registered "+user.getEmail());
             return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
         } catch (Exception e) {
+            logger.error("Error while registering user "+user.getEmail()+". Error: "+ e.getMessage());
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -47,12 +53,13 @@ public class AuthController {
     public ResponseEntity<Optional<RegisteredUser>> login(@RequestBody RegisteredUser user) throws Exception {
         try {
             authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword())
-            );
+                    new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword()));
         } catch (Exception e) {
+            logger.error("Error while logging in user "+user.getEmail()+". Error: "+ e.getMessage());
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid username or password");
         }
         final Optional<RegisteredUser> registeredUser = registeredUserService.findByEmail(user.getEmail());
+        logger.debug("Authenticated user "+user.getEmail());
         return new ResponseEntity<>(registeredUser, HttpStatus.OK);
     }
 }
